fecdf36e6a8232af44dc5d22a4a358b65905332c
kungua <maguahongkun@outlook.com>
2019-08-19 17:23:56 +0800

    添加 html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    'gui-react': './lib/index.tsx'
+    "gui-react": "./lib/index.tsx"
   },
   output: {
-    path: path.resolve(__dirname, 'dist/lib'),
-    library: 'gui-react',
-    libraryTarget: 'umd',
+    path: path.resolve(__dirname, "dist/lib"),
+    library: "gui-react",
+    libraryTarget: "umd"
   },
   module: {
-    rules: [
-      {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
-    ]
-  }
-}
+    rules: [{ test: /\.tsx?$/, loader: "awesome-typescript-loader" }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "GUI - React",
+      template: "index.html"
+    })
+  ]
+};
