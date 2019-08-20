eb90acbf4cc9a1f161247748f16c620cd0496007
kungua <maguahongkun@outlook.com>
2019-08-20 17:57:40 +0800

    添加 icon 的 click 事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,12 +3,13 @@ import './importIcons';
 import './icon.scss';
 
 interface iconProps {
-  name: string
+  name: string,
+  onClick: () => void
 }
 
 const Icon: React.FunctionComponent<iconProps> = (props) => {
   return (
-    <svg className="gui-icon">
+    <svg className="gui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}></use>
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,11 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import Icon from "./icon";
 
+const fn = () => {
+  console.log('fn')
+}
+
 ReactDOM.render(<div>
-  <Icon name="wechat-fill"/>
-  <Icon name="QQ"/>
-  <Icon name="alipay"/>
+  <Icon name="wechat-fill" onClick={fn}/>
+
 </div>, document.querySelector('#app'));
